6995859226e606d9b235bba2939fe27816a58d58
Vincent Laude <vincent.laude2@gmail.com>
2019-12-14 23:09:35 +0100

    Feat: generate access token

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,9 +1,10 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
-import { UserInputError } from 'apollo-server-errors';
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
+import { UserInputError, AuthenticationError } from 'apollo-server-errors';
 import { RegisterInput } from './dto/register.input';
 import { AuthResponse } from './dto/auth-response';
 import { UserService } from './user.service';
 import { AuthService } from './auth.service';
+import { LoginInput } from './dto/login.input';
 
 @Resolver(of => AuthResponse)
 export class AuthResolver {
@@ -21,5 +22,18 @@ export class AuthResolver {
         }
 
         return this.authService.register(registerData);
+    }
+
+    @Query(returns => AuthResponse)
+    async login(@Args('loginData') loginData: LoginInput): Promise<AuthResponse> {
+        const user = await this.userService.findOneByUsername(loginData.username);
+        if (!user) {
+            throw new AuthenticationError('Username or password incorrect');
+        }
+        const verifyPassword = await this.authService.verifyPassword(user, loginData.password);
+        if (!verifyPassword) {
+            throw new AuthenticationError('Username or password incorrect');
+        }
+        return this.authService.generateAccessToken(user, 'Authentication succeed');
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { RegisterInput } from './dto/register.input';
 import { AuthResponse } from './dto/auth-response';
 import { sign } from 'jsonwebtoken';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,14 +14,32 @@ export class AuthService {
     constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
 
     async register(registerData: RegisterInput): Promise<AuthResponse> {
+        // TODO Return user, and generate token in resolver with a generateAccessToken function call
         const createUser = this.userRepository.create(registerData);
         const user = await this.userRepository.save(createUser);
 
+        return this.generateAccessToken(user, `User ${user.username} created`);
+    }
+
+    async verifyPassword(user: User, password: string): Promise<boolean> {
+        // First, retreive hashed password
+        const hash = await this.userRepository
+            .createQueryBuilder()
+            .addSelect('password')
+            .select('password')
+            .where('id = :id', { id: user.id })
+            .getRawOne();
+
+        // Compare
+        return compare(password, hash.password);
+    }
+
+    async generateAccessToken(user: User, message?: string): Promise<AuthResponse> {
         // Create a JWT with jsonwebtoken
         const jwt = sign({ id: user.id }, this.JWT_SECRET_KEY, { expiresIn: 3600 });
         return {
             token: jwt,
-            message: `User ${user.username} created`,
+            message,
         };
     }
 }
