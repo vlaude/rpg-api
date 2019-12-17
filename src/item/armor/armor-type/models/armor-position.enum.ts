8fce2732ebb40e053d84bb016bc36fc3062620d9
Vincent Laude <vincent.laude2@gmail.com>
2019-12-17 21:34:50 +0100

    Add Shoulder armor position

--- src/item/armor/armor-type/models/armor-position.enum.ts
+++ src/item/armor/armor-type/models/armor-position.enum.ts
@@ -3,6 +3,7 @@ import { registerEnumType } from 'type-graphql';
 export enum ArmorPosition {
     HEAD = 'head',
     CHEST = 'chest',
+    SHOULDERS = 'shoulders',
     GREAVES = 'greaves',
     BOOTS = 'boots',
     GLOVES = 'gloves',
